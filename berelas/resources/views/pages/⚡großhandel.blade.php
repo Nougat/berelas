d5bc3d553f04d78254bd93c121ce24d8ae1e0efd
Nougat <raphael.moelck@hotmail.de>
2026-07-21 01:02:05 +0200

    started porting to laravel

--- berelas/resources/views/pages/⚡großhandel.blade.php
+++ berelas/resources/views/pages/⚡großhandel.blade.php
@@ -7,6 +7,9 @@ new class extends Component
 {
     public array $items = [];
 
+    public string $errorMessage;
+    public bool $isLoading;
+
     public function getItems(Großhandel $gh) 
     {
         $this->items = $gh->getItems();
@@ -15,17 +18,67 @@ new class extends Component
 };
 ?>
 
-<div>
+<div class="min-h-screen bg-gray-100 flex items-center justify-center p-6">
 
+    {{-- Container --}}
+    <div class="max-w-5xl w-full bg-white shadow-lg rounded-xl p-6">
 
-    <button wire:click="getItems" class="bg-blue-400 rounded text-white p-2">
-        Get Items
-    </button>
-    <span> {{ count($items) }} items loaded </span>
+        <!-- Error -->
+        @if ($errorMessage)
+        <div class="text-2xl text-red-500">{{ $errorMessage }}</div>
+        @endif
 
-    @foreach($items as $item)
-        <div class="p-4 border rounded mb-2">
-            {{ var_dump($item) }}
+        {{-- Headline --}}
+        <div class="text-center mb-6" wire:show='!items.length'>
+            <h1 class="text-3xl font-bold text-gray-900">Großhandel</h1>
+            <p class="mt-2 text-gray-600">This page loads the Google Sheet data directly in browser Javascript.</p>
         </div>
-    @endforeach
+
+        {{-- Load/Reload Button --}}
+        <div class="mb-6 space-y-4">
+            <button wire:click="getItems" wire:bind:disabled="isLoading" 
+                    class="w-full py-3 px-4  bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+                    wire:text="isLoading ? 'Loading...' : (items.length ? 'Reload sheet data' : 'Load sheet data')">
+            </button>
+        </div>
+
+        {{-- Search input and some simple filters --}}
+        <div class="mb-6">
+            
+            <input type="text" placeholder="Search..." wire:model="filterValues.search" wire:show="items.length"
+                    class="w-full border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            
+            <div wire:show="items.length" class="w-full flex justify-between">
+                <label>
+                    <input type="checkbox" wire:model="filterValues.comments" />
+                    Show only comments
+                </label>
+                    
+                <label>
+                    <input type="checkbox" wire:model="filterValues.orphanPrices" />
+                    Show only orphan prices
+                </label>
+                    
+                <label>
+                    <input type="checkbox" wire:model="filterValues.nonEmpty" />
+                    Show only non-empty
+                </label>
+                    
+                <label>
+                    <input type="checkbox" wire:model="filterValues.kleinanzeigen">
+                    Show only Kleinanzeigen
+                </label>
+                    
+                <label>
+                    <input type="checkbox" wire:model="filterValues.notListed">
+                    Show only not listed
+                </label>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
 </div>
